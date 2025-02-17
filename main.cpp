572830c348aead1e202c785619d3a780f2b21f66
Cao Thế Mạnh <manhcao47383@gmail.com>
2025-02-17 22:10:11 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
-#include <string>
+#include <conio.h>
+#include <windows.h>
+#include <ctime>
+using namespace std;
+struct Snake {
+ int x, y;
+};
+const int WIDTH = 40;
+const int HEIGHT = 20;
+const int INITIAL_SPEED = 50;
+const int MAX_LENGTH = 100;
+
+Snake snake[MAX_LENGTH];
+int nTail = 1;
+int fruitX, fruitY;
+int score = 0;
+int speed;
+enum Direction { STOP = 0, LEFT, RIGHT, UP, DOWN } dir;
+bool gameOver;
+
 using namespace std;
 int main() {
     cout<<"Game Over!!!";
