7f6422dfdeb9a548843925d48d7e4097aba0e148
Cao Thế Mạnh <manhcao47383@gmail.com>
2025-02-24 10:43:07 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,24 +3,21 @@
 #include <windows.h>
 #include <ctime>
 using namespace std;
+
 struct Snake {
- int x, y;
+    int x, y;
 };
 const int WIDTH = 40;
 const int HEIGHT = 20;
 const int INITIAL_SPEED = 50;
 const int MAX_LENGTH = 100;
-
 Snake snake[MAX_LENGTH];
 int nTail = 1;
-int fruitX, fruitY;
-int score = 0;
 int speed;
-enum Direction { STOP = 0, LEFT, RIGHT, UP, DOWN } dir;
-bool gameOver;
-
-using namespace std;
-int main() {
-    cout<<"Game Over!!!";
-    return 0;
+enum Direction {STOP = 0,LEFT ,RIGHT,UP ,DOWN } dir;
+void gotoXY(int x,int y){
+ COORD coord;
+ coord.X=x;
+ coord.Y=y;
+ SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 }
