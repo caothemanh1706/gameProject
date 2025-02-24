3ca871f2c4e834e24423f35af8c7a88ec842a1bf
Cao Thế Mạnh <manhcao47383@gmail.com>
2025-02-24 16:43:05 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <conio.h>
 #include <windows.h>
-#include <ctime>
 using namespace std;
 
 struct Snake {
@@ -10,8 +9,7 @@ struct Snake {
 const int WIDTH = 40;
 const int HEIGHT = 20;
 const int INITIAL_SPEED = 50;
-const int MAX_LENGTH = 100;
-Snake snake[MAX_LENGTH];
+Snake snake[1];
 int nTail = 1;
 int speed;
 enum Direction {STOP = 0,LEFT ,RIGHT,UP ,DOWN } dir;
